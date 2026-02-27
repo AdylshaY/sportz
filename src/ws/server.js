16d80dfcfb1f38dd6e8a3b2475e18c5cfb24d575
AdylshaY <adylshay@gmail.com>
2026-02-27 13:27:13 +0300

    feat: implement heartbeat mechanism for WebSocket connections

--- src/ws/server.js
+++ src/ws/server.js
@@ -21,6 +21,11 @@ export function attachWebSocketServer(server) {
   });
 
   wss.on('connection', (socket) => {
+    socket.isAlive = true;
+    socket.on('pong', () => {
+      socket.isAlive = true;
+    });
+
     sendJson(socket, {
       type: 'welcome',
       message: 'Welcome to the WebSocket server!',
@@ -28,6 +33,17 @@ export function attachWebSocketServer(server) {
 
     socket.on('error', console.error);
   });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 30000);
+
+  wss.on('close', () => clearInterval(interval));
 
   function broadcastMatchCreated(match) {
     broadcast(wss, { type: 'match_created', data: match });
